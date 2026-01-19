959ad76c454304ae46a96e4a927f14e7c96309cd
matthewsparkes <matthewsparkes14@gmail.com>
2026-01-19 11:23:20 +0000

    static methods

---
+++ static methods.py
@@ -0,0 +1,34 @@
+# Static methods = A method that belongs to a class rather than any object from that class (instance)
+#                  Usually used for general utitlity functions
+
+# Instance methods = Best for ooperations on instances of the class (objects)
+# Static methods = Best for utility functions that do not need accesss to class data
+
+class Employee:
+
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    def get_info(self):
+        return f"{self.name} = {self.position}"
+    
+    @staticmethod
+    def is_valid_position(position):
+        valid_positions = ["Manager", "Cashier", "Cook", "Janitor"]
+        return position in valid_positions
+    
+
+employee1 = Employee("Eugene", "Manager")
+employee2 = Employee("Squidward", "Cashier")
+employee3 = Employee("Spongebob", "Cook")
+
+print(employee3.get_info())
+
+# on an Instance method, you access an object, then call the instance method
+
+
+
+print(Employee.is_valid_position("Cook"))
+
+# on a Static method, you only need to access that class. You don't even need to create any objects from that class
