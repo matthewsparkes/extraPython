1376b66ab5bc485327de71a363fe06bc37fa360a
matthewsparkes <matthewsparkes14@gmail.com>
2026-01-24 21:59:05 +0000

    file detection

--- brocode_lessons/file-detection/file-detection1.py
+++ brocode_lessons/file-detection/file-detection1.py
@@ -3,9 +3,15 @@
 
 import os
 
-file_path = "test.txt"
+file_path = "file-detection/test.txt"
 
 if os.path.exists(file_path):
     print(f"The location '{file_path}' exists")
+
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+        
 else:
     print("That location doesn't exist")
